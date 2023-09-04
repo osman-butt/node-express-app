201fb3a1a2d8a32c2825b9fb3592df0df9a6f203
osman-butt <butt.osman@gmail.com>
2023-09-04 12:00:28 +0200

    added express Routes

---
+++ backend/routes/favouriteArtists.js
@@ -0,0 +1,6 @@
+import express from "express";
+import cors from "cors";
+import fs from "fs/promises";
+import cookieParser from "cookie-parser";
+
+const favouriteArtistsRouter = express.Router();
