a750749a5f2efd9227e58ac3df21bc7912dfa2fe
osman-butt <butt.osman@gmail.com>
2023-09-06 23:58:22 +0200

    added query support to artists data

--- backend/routes/artists.js
+++ backend/routes/artists.js
@@ -1,16 +1,26 @@
 import express from "express";
-import { getArtists, saveArtists } from "./helpers.js";
+import { getArtists, saveArtists, checkQuery } from "./helpers.js";
 
 const artistRouter = express.Router();
-
 artistRouter.get("/", async (req, res) => {
+  const queryObj = req.query;
+  const queryKeys = Object.keys(req.query);
+  // console.log("query:", queryObj);
   const artists = await getArtists();
-  res.json(artists);
+  if (queryKeys.length === 0) {
+    res.json(artists);
+  } else {
+    const filteredArtists = checkQuery(artists, queryObj);
+    res.send(filteredArtists);
+  }
 });
 
 artistRouter.post("/", async (req, res) => {
+  console.log("ARTISTS POST");
   const newArtist = req.body;
+  console.log(newArtist);
   newArtist.id = new Date().getTime();
+  console.log(newArtist);
   const artists = await getArtists();
   artists.push(newArtist);
   await saveArtists(artists);
@@ -31,14 +41,16 @@ artistRouter.put("/:id", async (req, res) => {
   artistToBeUpdated.website = updatedArtist.website;
   artistToBeUpdated.image = updatedArtist.image;
   artistToBeUpdated.shortDescription = updatedArtist.shortDescription;
+  artistToBeUpdated.spotifyLink = updatedArtist.spotifyLink;
   await saveArtists(artists);
   res.json(artists);
 });
 
 artistRouter.delete("/:id", async (req, res) => {
   const id = Number(req.params.id);
+  console.log(id);
   const artists = await getArtists();
-  const updatedArtists = artists.filter(artist => artist.id !== id);
+  const updatedArtists = artists.filter(artist => artist.id !== Number(id));
   await saveArtists(updatedArtists);
   res.json(updatedArtists);
 });
--- backend/routes/helpers.js
+++ backend/routes/helpers.js
@@ -21,4 +21,81 @@ async function saveArtists(data) {
   await fs.writeFile("artists.json", JSON.stringify(data));
 }
 
-export { getArtists, saveArtists, getFavourites, saveFavourites };
+function getRoute(req, res, next) {
+  console.log(req.method, ":", req.originalUrl);
+  next();
+}
+
+function checkQuery(artistsObj, queryObj) {
+  // NOTE THIS FUNCTION ASSUMES THAT PROPERTIES IN BOTH INPUT OBJECTS,
+  // DOES NOT CONTAIN ANY NUMBER!!!
+
+  // INPUT:
+  // artistsObj: Full artists list
+  // queryObj: result from req.query
+
+  // OUTPUT:
+  // All artists that meets the query constraints. See example
+
+  // EXAMPLE 1:
+  // query artists?genres=Rock,Soul&labels=Columbia
+  // queryObj = {genres: 'Rock,Soul', labels: 'Columbia'}
+  // OUTPUT: Should be all artists with genres Rock or Soul and label = Columbia
+
+  // EXAMPLE 2:
+  // query artists?genres=Rock,Soul&name=Eminem
+  // queryObj = {genres: 'Rap,Soul', name: 'Eminem'}
+  // OUTPUT: Should be all artists with genres Rap or Soul and name = Eminem
+
+  // (*)
+  // Query: artists?genres=Rap&genres=Soul&labels=Columbia need to be handled.
+  // So we convert {genres: ['Rap','Soul'], labels: 'Columbia'} to
+  // {labels: 'Columbia', genres0: 'Rap', genres1: 'Soul'}
+
+  // This loop creates (*)
+  for (const key in queryObj) {
+    // If it's an array we flatten the object, and append a number
+    if (typeof queryObj[key] === "object") {
+      // console.log(queryObj[key].length);
+      for (let i = 0; i < queryObj[key].length; i++) {
+        queryObj[key + i] = queryObj[key][i];
+      }
+      delete queryObj[key];
+    }
+  }
+
+  // Each entry is a list of artists that satisfies each query, ie
+  // a list artists where AND is replaced with OR in the query.
+  const unionFilteredArtists = [];
+  for (const query in queryObj) {
+    const cleanedQuery = query.replace(/[0-9]/g, "");
+    if (typeof artistsObj[0][cleanedQuery] === "object") {
+      // Remove appended number
+      const listOfArtists = artistsObj.filter(artist =>
+        artist[cleanedQuery].some(artistQuery =>
+          queryObj[query].split(",").includes(artistQuery)
+        )
+      );
+      unionFilteredArtists.push(listOfArtists);
+    } else {
+      const listOfArtists = artistsObj.filter(artist =>
+        queryObj[query].split(",").includes(artist[cleanedQuery])
+      );
+      unionFilteredArtists.push(listOfArtists);
+    }
+  }
+  // We get the intersection of all artists arrays, ie. this is the & (AND) in the query
+  const intersectionFilteredArtists = unionFilteredArtists.reduce((a, b) =>
+    a.filter(c => b.includes(c))
+  );
+  return intersectionFilteredArtists;
+}
+
+export {
+  getArtists,
+  saveArtists,
+  getFavourites,
+  saveFavourites,
+  getRoute,
+  checkQuery,
+};
