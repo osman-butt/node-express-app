edb4c6734e85c7dc7bb6a66da2fcc5cc62cccd11
osman-butt <butt.osman@gmail.com>
2023-09-06 23:59:33 +0200

    Added logger and genreRoute

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,11 @@ import cookieParser from "cookie-parser";
 import { artistRouter } from "./routes/artists.js";
 import { favouritesRouter } from "./routes/favourites.js";
 import { favouriteArtistsRouter } from "./routes/favouriteArtists.js";
+import { genreRouter } from "./routes/genres.js";
 import { retrieveCookies } from "./cookies.js";
+import { getRoute } from "./routes/helpers.js";
+import morgan from "morgan";
+import fsSync from "fs";
 
 // Globals
 const port = 3000;
@@ -14,12 +18,37 @@ const host = "http://localhost";
 const app = express();
 // Add middleware
 app.use(express.json()); // parse incomming JSON
-app.use(cors({ origin: "http://127.0.0.1:8080", credentials: true })); // Using CORS + settings
+app.use(
+  cors({
+    origin: ["http://127.0.0.1:8080", "http://127.0.0.1:5500"],
+    credentials: true,
+  })
+); // Using CORS + settings
 app.use(cookieParser()); // Using cookies
+
+// create a write stream (in append mode)
+const accessLogStream = fsSync.createWriteStream("requests.log", {
+  flags: "a",
+});
+
+// Define a custom log format that includes cookies
+morgan.token("cookies", req => {
+  return req.cookies.session;
+});
+
+// Create a custom format that logs cookies along with other request information
+const customFormat =
+  ":remote-addr - :remote-user [:date[clf]] ':method :url HTTP/:http-version' :status :res[content-length] - :response-time ms ':referrer' ':user-agent' Session: :cookies";
+
+// setup the logger
+app.use(morgan(customFormat, { stream: accessLogStream }));
+// app.use(morgan("combined", { stream: accessLogStream }));
 app.use(retrieveCookies); // Set & get cookies
+app.use(getRoute);
 app.use("/artists", artistRouter);
 app.use("/favourites", favouritesRouter);
 app.use("/favouriteartists", favouriteArtistsRouter);
+app.use("/genres", genreRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to MusicBase API");
